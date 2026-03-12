073938a02b99c843618b1c71ce83ef9df5b48002
Mike Moate <mike.moate@countingup.com>
2026-03-12 12:10:48 +0000

    Update to Go 1.25
    Rename test case as otherwise `go test` expects a `Simple` function to exist (https://go.dev/blog/examples)

--- example_test.go
+++ example_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/kinesis"
 )
 
-func ExampleSimple() {
+func SimpleExample() {
 	logger := &StdLogger{log.New(os.Stdout, "", log.LstdFlags)}
 	cfg, _ := config.LoadDefaultConfig(context.TODO())
 	client := kinesis.NewFromConfig(cfg)
